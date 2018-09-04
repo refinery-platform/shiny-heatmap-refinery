efc986aa1195201abd17d45a44f54bf9cd4d3171
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-09-04 15:47:21 -0400

    Wait longer on travis than locally; install.packages on own line; fix test

--- test.sh
+++ test.sh
@@ -5,10 +5,12 @@ set -o errexit
 start() { echo travis_fold':'start:$1; echo $1; set -v; }
 end() { set +v; echo travis_fold':'end:$1; echo; echo; }
 retry() {
-    TRIES=1
+    TRY=1
     until curl --silent --fail http://localhost:$PORT/ > /tmp/response.txt; do
-        echo "$TRIES: not up yet"
-        if (( $TRIES > 10 )); then
+        echo "$TRY: not up yet"
+        MAX_TRY=3
+        if [ -z "$TRAVIS" ]; then MAX_TRY=10; fi
+        if (( $TRY > $MAX_TRY )); then
             echo '----------------'
             echo 'docker logs:'
             $OPT_SUDO docker logs $CONTAINER_NAME
@@ -22,7 +24,7 @@ retry() {
             echo '----------------'
             die "HTTP requests to app never succeeded"
         fi
-        (( TRIES++ ))
+        (( TRY++ ))
         sleep 1
     done
     echo 'Container responded with:'
@@ -41,7 +43,7 @@ start docker_run
 retry
 echo "docker is responsive"
 ACTUAL_TEXT=`curl http://localhost:8888/`
-grep 'TODO: heatmap ... ???' <(echo "$ACTUAL_TEXT") || die "No match: $ACTUAL_TEXT"
+grep 'Hello World!' <(echo "$ACTUAL_TEXT") || die "No match: $ACTUAL_TEXT"
 
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
